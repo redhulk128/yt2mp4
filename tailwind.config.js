2462c13473b42ac5ca76d3a6a7f56abc089be7ce
redhulkk <140883969+redhulkk@users.noreply.github.com>
2023-10-29 00:30:47 +0530

    yt2mp4

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  darkMode : 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        'robotos': ['Roboto Slab'],
+        'ubuntu': ['Ubuntu'],
+        'montserrat': ['Montserrat'],
+      }
+    },
+  },
+  plugins: [],
+}
+
+
+  
